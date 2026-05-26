f6f86f85ccdc7c9181294eec71b301998d2e5c7a
EvandroStk9 <evandroluisalves13@gmail.com>
2026-05-26 18:09:53 -0300

    Corrigindo deploy: instalando dependências de sistema

--- main.R
+++ main.R
@@ -5,9 +5,3 @@ source(here("src/01_ingest.R"))
 source(here("src/02_etl.R"))
 source(here("src/03_modules_gen.R"))
 shiny::runApp(here("app/app.R"))
-
-# Deploy no shinyapps.io
-rsconnect::deployApp(
-  appDir = here::here("app"), 
-  appName = "mortes_evitaveis_por_raca"
-  )
